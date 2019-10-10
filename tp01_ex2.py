54392276fee0056ec0a243617cfe9f40b5a9bb6e
hanaharun <55502252+hanaharun@users.noreply.github.com>
2019-10-10 13:55:01 +0200

    Add files via upload

--- tp01_ex2.py
+++ tp01_ex2.py
@@ -17,31 +17,11 @@ Programme simulant un distributeur de snacks
 
 ### Déclaration des variables
 
-SANDWICH: float = 4.90
-CHIPS: float = 2.50
-CHOCOLAT: float = 2.00
-BONBONS: float = 3.30
-TEA: float = 2.20
-LIMONADE: float = 1.90
-sandwich_poulet: int = 1
-chips_paprika: int = 2
-barre_choco: int = 3
-bonbon: int = 4
-ice_tea: int = 5
-limonade: int = 6
-monnaie: float = 0.0
-produit: str
-rendu: float = 0.0
-prix = float = 0.0
-nom_produit: str
-phrase: str
 
 
 ###Initialisation des variables
 
-nom_produit = ""
-monnaie: float = float(input("Veuillez introduire votre monnaie : "))
-produit: str = str(input("Veuillez sélectionner un produit : "))
+
 
 ### Séquence d'opération
 
@@ -53,40 +33,3 @@ print("3. Barre chocolatée")
 print("4. Bonbons")
 print("5. Ice Tea")
 print("6. Limonade")
-
-
-if produit == 1:
-    prix = 4.90
-    nom_prod = "Sandwich au poulet"
-elif produit == 2:
-    prix = 2.50
-    nom_produit = "Chips Paprika"
-elif produit == 3:
-    prix = 2.00
-    nom_prdotuit = "Barre chocolatée"
-elif produit == 4:
-    prix = 3.30
-    nom_produit = "Bonbons"
-elif produit == 5:
-    prix = 2.20
-    nom_produit = "Ice Tea"
-elif produit == 6:
-    prix = 1.90
-    nom_produit = "Limonade"
-
-
-
-if monnaie > prix:
-    rendu = monnaie - prix
-    print("Monnaie à rendre : ", round(rendu, 2))
-    print(nom_produit, phrase)
-else:
-    print(nom_produit, phrase)
-
-
-
-
-
-
-
-
